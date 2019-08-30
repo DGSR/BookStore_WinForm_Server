5d1e94c32d6e0cb6bd4d8df443ff7cf6f399bde1
DGSR <33521824+DGSR@users.noreply.github.com>
2019-08-30 14:45:04 +0300

    Update AboutBox.cs

--- BookShop/AboutBox.cs
+++ BookShop/AboutBox.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
@@ -14,16 +14,15 @@ namespace BookShop
         public AboutBox()
         {
             InitializeComponent();
-            this.Text = String.Format("О программе {0}", AssemblyTitle);
+            this.Text = String.Format("About {0}", AssemblyTitle);
             this.labelProductName.Text = AssemblyProduct;
-            this.labelVersion.Text = String.Format("Версия {0}", AssemblyVersion);
+            this.labelVersion.Text = String.Format("Version {0}", AssemblyVersion);
             this.labelCopyright.Text = AssemblyCopyright;
             this.labelCompanyName.Text = AssemblyCompany;
             this.textBoxDescription.Text = AssemblyDescription;
         }
 
         #region 
-
         public string AssemblyTitle
         {
             get
